673c46cee4b33e571c012772003514d2633d9d52
Ginnyelf <ginnyelf0801@gmail.com>
2023-05-14 15:33:31 +0300

    Second commit for branch testng

--- src/test/java/ru/netology/basic2/CashBackHackerTest.java
+++ src/test/java/ru/netology/basic2/CashBackHackerTest.java
@@ -1,37 +1,35 @@
 package ru.netology.basic2;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
-class CashBackHackerTest {
-    @Test
-    void shouldReturnBoundary() {
+public class CashBackHackerTest {
+    @org.testng.annotations.Test
+     public void shouldReturnBoundary() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 900;
         int expected = 100;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
 
     }
 
-    @Test
-    void shouldReturnBoundaryISNull() {
+    @org.testng.annotations.Test
+     public void shouldReturnBoundaryISNull() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 1000;
         int expected = 0;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
 
     }
 
-    @Test
-    void shouldReturnBoundaryISNull2() {
+    @org.testng.annotations.Test
+    public void shouldReturnBoundaryISNull2() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 2000;
         int expected = 0;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.testng.Assert.assertEquals(actual, expected);
 
     }
 
