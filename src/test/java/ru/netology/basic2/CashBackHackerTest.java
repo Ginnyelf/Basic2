6468156ebefce52afa3317e52391fbe6a58f03dc
Ginnyelf <ginnyelf0801@gmail.com>
2023-05-14 15:47:27 +0300

    Third commit for branch junit4

--- src/test/java/ru/netology/basic2/CashBackHackerTest.java
+++ src/test/java/ru/netology/basic2/CashBackHackerTest.java
@@ -1,37 +1,34 @@
 package ru.netology.basic2;
 
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class CashBackHackerTest {
-    @Test
-    void shouldReturnBoundary() {
+public class CashBackHackerTest {
+    @org.junit.Test
+    public void shouldReturnBoundary() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 900;
         int expected = 100;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
 
     }
 
-    @Test
-    void shouldReturnBoundaryISNull() {
+    @org.junit.Test
+    public void shouldReturnBoundaryISNull() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 1000;
         int expected = 0;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
 
     }
 
-    @Test
-    void shouldReturnBoundaryISNull2() {
+    @org.junit.Test
+    public void shouldReturnBoundaryISNull2() {
         CashBackHacker cashBackHacker = new CashBackHacker();
         int amount = 2000;
         int expected = 0;
         int actual = cashBackHacker.remain(amount);
-        assertEquals(expected, actual);
+        org.junit.Assert.assertEquals(expected, actual);
 
     }
 
