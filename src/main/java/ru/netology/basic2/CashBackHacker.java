0ce25947db187e199ff8cf11a0da73649cf3d75a
Ginnyelf <ginnyelf0801@gmail.com>
2023-05-15 16:52:43 +0300

    FallTest

--- src/main/java/ru/netology/basic2/CashBackHacker.java
+++ src/main/java/ru/netology/basic2/CashBackHacker.java
@@ -1,15 +1,12 @@
 package ru.netology.basic2;
 
 public class CashBackHacker {
+
+
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount/ boundary >= 1) {
-            int boundary = 0;
-            return boundary;
-        }
-
-
         return boundary - amount % boundary;
     }
 }
+
